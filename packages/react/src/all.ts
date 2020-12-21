0ffdf9f5ed5f6270fd15606e436fcf2cb79e94cc
AppleMonkey2019 <am_fe@qq.com>
2020-12-21 19:33:15 +0800

    fix: 修复 `React Package` IconType类型问题.[#61]

--- packages/react/src/all.ts
+++ packages/react/src/all.ts
@@ -2,9 +2,11 @@
  * @file all
  */
 
-import * as icons from './map';
+import * as IconMap from './map';
 import {IIconProps} from './runtime';
 import React from 'react';
+
+export type IconType = keyof typeof IconMap
 
 /**
  * 获取对象的属性名数组
@@ -15,19 +17,20 @@ function getKeys<T>(obj: T): Array<keyof T> {
     return Object.keys(obj) as Array<keyof T>;
 }
 
-export const ALL_ICON_KEYS = getKeys(icons);
+export const ALL_ICON_KEYS = getKeys(IconMap);
 
-export type IconType = typeof ALL_ICON_KEYS;
-
-export default function Icon(props: {type: keyof typeof icons} & IIconProps) {
+export interface IIconAllProps extends IIconProps {
+    type: IconType
+}
+export default function Icon(props: IIconAllProps) {
 
     const {type, ...extra} = props;
 
-    if (!(type in icons)) {
+    if (!(type in IconMap)) {
         throw new Error(`${type} is not a valid icon type name`);
     }
 
-    return React.createElement(icons[type], extra);
+    return React.createElement(IconMap[type], extra);
 }
 
 export * from './runtime';
